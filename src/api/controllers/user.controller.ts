3a22f69a9205c83ffcab753223ee75d82fd93a4f
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-04-04 18:44:11 +0600

    update: storing password with hashed

--- src/api/controllers/user.controller.ts
+++ src/api/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { User } from "../models/user.model";
 
@@ -16,7 +17,9 @@ export const userController = {
 
   createUser: async (req: Request, res: Response) => {
     try {
-      const user = new User(req.body);
+      const { password, ...otherData } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ ...otherData, password: hashedPassword });
       const savedUser = await user.save();
       res.status(201).json(savedUser);
     } catch (error) {
