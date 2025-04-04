57ec5cffecffb0a7069dc7019ce66e22624eeb0c
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-04-04 15:52:24 +0600

    env related issue fixed

--- env.ts
+++ env.ts
@@ -1,1 +1,4 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
 export const MONGODB_URI = process.env.MONGODB_URI;
--- src/api/config/db.ts
+++ src/api/config/db.ts
@@ -1,12 +1,15 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { MONGODB_URI } from "../../../env";
 
-dotenv.config({ path: ".env.local" });
+const DB_NAME = "green-pulse";
 
 export const connectDB = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI as string);
+    const db = await mongoose.connect(MONGODB_URI as string, {
+      dbName: DB_NAME,
+    });
     console.log(`MongoDB Connected: ${db.connection.host}`);
+    return db;
   } catch (error) {
     console.error(`Error: ${error}`);
     process.exit(1);
