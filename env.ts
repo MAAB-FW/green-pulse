ffe338823ea07b2d6f3103005f0b81c797bd3013
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-04-04 16:26:56 +0600

    update: env updated

--- env.ts
+++ env.ts
@@ -2,3 +2,5 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
 export const MONGODB_URI = process.env.MONGODB_URI;
+export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
