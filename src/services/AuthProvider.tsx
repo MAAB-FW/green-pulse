e6936b59affb623b3cd078c2d649427d94943572
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-04-08 23:04:56 +0600

    update: AuthProvider implemented

---
+++ src/services/AuthProvider.tsx
@@ -0,0 +1,28 @@
+import { auth } from "@/firebase/firebase.config";
+import { setUser } from "@/redux/features/user/userSlice";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          setUser({
+            _id: user.uid,
+            name: user.displayName || "",
+            email: user.email || "",
+            isLoading: false,
+            isError: false,
+            error: "",
+          })
+        );
+      }
+    });
+  }, [dispatch]);
+  return children;
+};
+
+export default AuthProvider;
--- src/services/ReduxProvider.tsx
+++ src/services/ReduxProvider.tsx
@@ -3,9 +3,14 @@ import store from "@/redux/store";
 import { Provider } from "react-redux";
 
 import { ReactNode } from "react";
+import AuthProvider from "./AuthProvider";
 
 const ReduxProvider = ({ children }: { children: ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <AuthProvider>{children}</AuthProvider>
+    </Provider>
+  );
 };
 
 export default ReduxProvider;
