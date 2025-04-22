42ced9e6fb1729580f8f89a7d6fd4f6644d44990
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-04-22 23:16:50 +0600

    update: getting user role type in authProvider

--- src/services/AuthProvider.tsx
+++ src/services/AuthProvider.tsx
@@ -1,14 +1,32 @@
 import { auth } from "@/firebase/firebase.config";
 import { setUser } from "@/redux/features/user/userSlice";
+import { RootState } from "@/redux/store";
+import axios from "axios";
 import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NEXT_PUBLIC_API_URL } from "../../env";
 
 const AuthProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }): React.ReactNode => {
+  const { email } = useSelector((state: RootState) => state.userSlice);
+  const [, setRole] = useState<string>("");
+
+  useEffect(() => {
+    if (email) {
+      const getUserRoleType = async () => {
+        const { data } = await axios(
+          `${NEXT_PUBLIC_API_URL}/api/users/${email}`
+        );
+        return setRole(data);
+      };
+      getUserRoleType();
+    }
+  }, [email]);
+
   const dispatch = useDispatch();
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
